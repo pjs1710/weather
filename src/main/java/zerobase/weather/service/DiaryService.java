23bc122d44ab6f83fb327b629513cc455af7668c
Park Jae Seong <timer973@naver.com>
2025-08-09 01:44:20 +0900

    날씨 데이터 저장 API 작성_2

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,12 +1,22 @@
 package zerobase.weather.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
 @Service
 public class DiaryService {
+
+    @Value("${openweathermap.key}")
+    private String apiKey;
+
     public void createDiary(LocalDate date, String text) {
+
+    }
+
+    private String getWeatherString() {
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
 
     }
 }
